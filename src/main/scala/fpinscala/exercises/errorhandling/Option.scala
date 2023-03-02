b689b21a2e44495fdeffd8a46995fcd86a8ef2b7
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2023-03-02 18:44:38 +0100

    code on options
    Co-authored-by: Marc Farran Abad <mfa2001@users.noreply.github.com>
    Co-authored-by: SaraSerrano7 <SaraSerrano7@users.noreply.github.com>
    Co-authored-by: RobertTrif <RobertTrif@users.noreply.github.com>
    Co-authored-by: Aarón Arenas <aaron-at97@users.noreply.github.com>

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -7,38 +7,76 @@ enum Option[+A]:
   case Some(get: A)
   case None
 
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = this match
+    case None    => None
+    case Some(a) => Some(f(a))
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = this match
+    case None    => None
+    case Some(a) => f(a)
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def fold[B](ifNone: => B, ifSome: A => B): B = this match
+    case None    => ifNone
+    case Some(a) => ifSome(a)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def filter(f: A => Boolean): Option[A] = this match
+    case sa @ Some(a) if f(a) => sa
+    case _                    => None
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def getOrElse[B >: A](default: => B): B = this match
+    case Some(a) => a
+    case None    => default
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match
+    case Some(_) => this
+    case None    => ob
+
+  def flatMapGOE[B](f: A => Option[B]): Option[B] =
+    this.map(f).getOrElse(None)
+
+  def filterFM(f: A => Boolean): Option[A] =
+    flatMap(a => if f(a) then Some(a) else None)
+
+  def orElseGOE[B >: A](ob: => Option[B]): Option[B] =
+    this.map(Some(_)).getOrElse(ob)
 
 object Option:
 
   def failingFn(i: Int): Int =
-    val y: Int = throw new Exception("fail!") // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
+    val y: Int =
+      throw new Exception(
+        "fail!"
+      ) // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
     try
       val x = 42 + 5
       x + y
-    catch case e: Exception => 43 // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
+    catch
+      case e: Exception =>
+        43 // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
 
   def failingFn2(i: Int): Int =
     try
       val x = 42 + 5
-      x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
+      x + ((throw new Exception(
+        "fail!"
+      )): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
     catch case e: Exception => 43
 
-  def mean(xs: Seq[Double]): Option[Double] =
+  def mean(xs: List[Double]): Option[Double] =
     if xs.isEmpty then None
     else Some(xs.sum / xs.length)
 
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: List[Double]): Option[Double] =
+    // If mean signaled errors via exceptions the code would have been:
+    // val m = mean(xs)
+    // val ts = xs.map(x => math.pow(x - m, 2))
+    // mean(ts)
+    mean(xs).flatMap { m =>
+      val ts = xs.map(x => math.pow(x - m, 2))
+      mean(ts)
+    }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
 
   def sequence[A](as: List[Option[A]]): Option[List[A]] = ???
 
--- src/main/scala/fpinscala/worksheets/option_variance.worksheet.sc
+++ src/main/scala/fpinscala/worksheets/option_variance.worksheet.sc
@@ -1,28 +1,30 @@
-// enum MyOption[+A]:
-//   case Some(get: A)
-//   case None
+enum MyOption[+A]:
+  case Some(get: A)
+  case None
 
-//   def getOrElse(default: => A): A = this match
-//     case Some(get) => get
-//     case None      => default
+  def getOrElse[B >: A](default: => B): B = this match
+    case Some(a) => a
+    case None    => default
 
-// trait Animal
-// class Cat extends Animal
-// class Dog extends Animal
+trait Animal
+class Cat extends Animal
+class Dog extends Animal
 
-// val maybeAnimal: MyOption[Animal] =
-//   MyOption.Some(new Dog)
+val animal: Animal = new Dog
 
-// val anotherMaybeAnimal: Animal =
-//   maybeAnimal.getOrElse(new Cat) // compiles
+val maybeAnimal: MyOption[Animal] =
+  MyOption.Some(new Dog)
+
+val anotherMaybeAnimal: Animal =
+  maybeAnimal.getOrElse(new Cat) // compiles
 
 // // If MyOption is covariant any MyOption[Dog] is valid as a MyOption[Animal]
 
-// val maybeDog: MyOption[Dog] =
-//   MyOption.Some(new Dog)
+val maybeDog: MyOption[Dog] =
+  MyOption.Some(new Dog)
 
-// // val anotherMaybeAnimal2: Animal =
-// //   maybeDog.getOrElse(new Cat) // does not compile
+val anotherMaybeAnimal2: Animal =
+  maybeDog.getOrElse(new Cat) // does not compile
 
 // def function(optionAnimal: MyOption[Animal]): Animal =
 //   optionAnimal.getOrElse(new Cat)
